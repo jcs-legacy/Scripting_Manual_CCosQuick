a97c32673885e993dad2401dd6003e09d9f5136e
jenchieh <jcs090218@gmail.com>
2020-01-12 12:39:41 +0800

    Update core.

---
+++ config.js
@@ -0,0 +1,48 @@
+/**
+ * $File: config.js $
+ * $Date: 2018-09-27 17:11:04 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2018 by Shen, Jen-Chieh $
+ */
+
+"use strict";
+
+
+const DEBUG = false;  // Debug mode?
+
+const HOST = "localhost";  // Host [Default: localhost]
+const PORT = 3005;         // Server Port [Default: 3000]
+
+const WEBSITE_DIR = "website";
+
+/* Paths */
+const API_DIR_PATH = "./website/ScriptReference/api/";
+const MANUAL_DIR_PATH = "./website/Manual/doc/";
+
+/* Action */
+const SORT_ORDER = 'directory';
+
+// Content extension to load.
+//
+// NOTE: have to be the same with 'client' side's config file.
+//
+// SEE : ./website/js/config.js
+const CONTENT_EXTENSION = /\.md/;
+
+//------------------ Exports modules ------------------//
+
+module.exports.DEBUG = DEBUG;
+
+module.exports.HOST = HOST;
+module.exports.PORT = PORT;
+
+module.exports.WEBSITE_DIR = WEBSITE_DIR;
+
+module.exports.API_DIR_PATH = API_DIR_PATH;
+module.exports.MANUAL_DIR_PATH = MANUAL_DIR_PATH;
+
+module.exports.SORT_ORDER = SORT_ORDER;
+
+module.exports.CONTENT_EXTENSION = CONTENT_EXTENSION;
